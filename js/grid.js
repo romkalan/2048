b9c4ceb75a2248176624cb79f17bb8e9fa9757e7
romkalan <romkalan@mail.ru>
2023-12-18 12:21:33 +0300

    add script for moving tiles and merge them

--- js/grid.js
+++ js/grid.js
@@ -11,11 +11,20 @@ export class Grid {
                 new Cell(gridElement, i % gridSize, Math.floor(i / gridSize) )
             );
         }
+        this.cellsGroupedByColumn = this.groupCellsByColumn();
     }
 
     getRandomEmptyCell() {
         const emptyCells = this.cells.filter(cell => cell.isEmpty());
         const randomID = Math.floor(Math.random() * emptyCells.length);
         return emptyCells[randomID];
+    }
+
+    groupCellsByColumn() {
+        return this.cells.reduce((groupedCells, cell) => {
+            groupedCells[cell.x] = groupedCells[cell.x] || [];
+            groupedCells[cell.x][cell.y] = cell;
+            return groupedCells;
+        }, []);
     }
 }
--- js/tile.js
+++ js/tile.js
@@ -2,7 +2,8 @@ export class Tile {
     constructor(gridElement) {
         this.tileElement = document.createElement("div");
         this.tileElement.classList.add("tile");
-        this.setValue(Math.random() > 0.5 ? 2 : 4)
+        this.setXY()
+        this.setValue(Math.random() > 0.5 ? 2 : 4);
         gridElement.append(this.tileElement);
     }
 
@@ -18,8 +19,12 @@ export class Tile {
         this.value = value;
         this.tileElement.textContent = value;
 
-        const bgLigtness = 100 - Math.log2(value) * 9;
-        this.tileElement.style.setProperty("--bgLigtness", `${bgLigtness}%`);
-        this.tileElement.style.setProperty("--textLightness", `${bgLigtness < 50 ? 90 : 10}%`);
+        const bgLightness = 100 - Math.log2(value) * 9;
+        this.tileElement.style.setProperty("--bgLightness", `${bgLightness}%`);
+        this.tileElement.style.setProperty("--textLightness", `${bgLightness < 50 ? 90 : 10}%`);
+    }
+
+    removeFromDOM() {
+        this.tileElement.remove();
     }
 }
